f62d27e7b21427cfacb83b14f625097899af03b1
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-04-21 17:01:46 +0300

    update EyeOnMe.js, Keypad.js components

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,25 @@
 // Code Keypad Component Here
+import React, { useState } from "react";
+
 
 function Keypad (){
+    const [password, setPassword] = useState("");
+    const handleChange = () => {
+
+        console.log("Entering password...");
+    };
+
     return (
-        <div></div>
+        <div>
+            <input
+                type="password" // Correct type for password input
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value); // Update state with input value
+                  handleChange(); // Trigger the event handler
+                }}
+            />
+        </div>
     )
 }
 
